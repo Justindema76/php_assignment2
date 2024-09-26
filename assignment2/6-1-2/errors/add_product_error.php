b0a648bbdc25aaadb6746e38c56e4c6029fb54b4
Justin DeMatteis <160768253+Justindema76@users.noreply.github.com>
2024-09-26 12:03:04 -0400

    Delete assignment2 directory

--- assignment2/6-1-2/errors/add_product_error.php
+++
@@ -1,7 +0,0 @@
-<?php include("view/header.php"); ?>
-<main>
-    <h1>Error</h1>
-    <p><?php echo $error; ?></p>
-    <p><a href="add_product_form.php">Back to Add Product Form</a></p>
-</main>
-<?php include 'view/footer.php'; ?>
